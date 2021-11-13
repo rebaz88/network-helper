587ce72b031f425a06d030b26331a938b8f27037
Rebaz Saleh <rebaz88.software@gmail.com>
2021-11-13 23:59:26 +0300

    wip

--- src/options/ping_all_devices.rs
+++ src/options/ping_all_devices.rs
@@ -3,31 +3,60 @@ use crate::utils::read_csv::Device;
 use crate::utils::pinger;
 use crate::utils::ip_validation::is_valid_ip_address;
 use std::error::Error;
+use std::fmt;
+
+pub struct PingAll<'a> {
+    pub valid_ips : Vec<&'a Device>,
+    invalid_ips : Vec<&'a Device>,
+    passed_ips:  Vec<&'a Device>,
+    failed_ips: Vec<&'a Device>, 
+}
+
+impl<'a> PingAll<'a> {
+    
+    fn new() -> PingAll<'a> {
+        PingAll {
+            valid_ips: Vec::new(),
+            invalid_ips: Vec::new(),
+            passed_ips: Vec::new(),
+            failed_ips: Vec::new(),
+        }
+    }
+
+    pub fn validate_all(&mut self) {
+    
+        let ips: Result<Vec<Device>, Box<dyn Error>> = read_csv::run();
+        
+        match ips {
+            Ok(data) => {
+                for ip in data.iter() {
+                    let d: &Device = ip;
+                    let validate = is_valid_ip_address(&ip.ip_address);
+                    if validate.is_valid {
+                        &self.valid_ips.push(d);
+                    } else {
+                        self.invalid_ips.push(d);
+                    }
+                }
+            },
+            Err(_) => panic!("Could not perform the operation!"),
+        }
+    }
+}
+
+impl fmt::Debug for PingAll<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("PingAll")
+         .field("valid_ips", &self.valid_ips)
+         .field("invalid_ips", &self.invalid_ips)
+         .field("passed_ips", &self.passed_ips)
+         .field("failed_ips", &self.failed_ips)
+         .finish()
+    }
+}
 
 pub fn run() {
-    let ips: Result<Vec<Device>, Box<dyn Error>> = read_csv::run();
-    
-    match ips {
-        Ok(data) => {
-            let mut valid_devices: Vec<&Device> = Vec::new();
-            for (i,ip) in data.iter().enumerate() {
-                let d: &Device = ip;
-                let validate = is_valid_ip_address(&ip.ip_address);
-                if validate.is_valid {
-                    valid_devices.push(d);
-                } else {
-                    println!("Invalid ip address found at line {}", i + 1);
-                }
-            }
-            
-            let valid_device_ips: Vec<&str> = valid_devices.iter()
-                .map(|d| d.ip_address.as_str())
-                .collect();
-            
-            pinger::run(&valid_device_ips);
-        },
-        Err(_) => panic!("Could not perform the operation!"),
-    }
-    
-    
+    let ping_all = PingAll::new().validate_all();
+    // println!("{:?}", ping_all.valid_ips);  
 }
+
